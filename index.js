955de4953b95b820cad1d9e75432cbcfc9449710
Emma King <emmailonaking@gmail.com>
2022-07-08 10:08:56 +0300

    inital

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+
+const app = express()
+const port = 3000
+
+//text based JSON to JS-accesible variables in req.body
+app.use(bodyParser.json())
+//req.body will be an object that contains values of any type
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+    res.send({ message: 'Can this actually work? Ihmettelen vaan. T. Emma' })
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+})
